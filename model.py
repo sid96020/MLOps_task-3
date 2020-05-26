27e88d1adcd41ec1fb34db2c79fbfbf0bedf0b0b
sid96020 <sid9602044416@gmail.com>
2020-05-26 08:52:24 +0530

    forth

--- model.py
+++ model.py
@@ -34,7 +34,7 @@ model = Sequential()
 model.add(Conv2D(filters=8, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
-model.add(Conv2D(filters=3, (3, 3), activation='relu'))
+model.add(Conv2D(filters=3,kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
