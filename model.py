f99584124538ac35416b22d0442ec5e0ea080ecb
sid96020 <sid9602044416@gmail.com>
2020-05-26 08:49:24 +0530

    third

--- model.py
+++ model.py
@@ -5,8 +5,6 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
-
-batch_size = 128
 num_classes = 10
 epochs=1
 
@@ -33,14 +31,14 @@ x_test /= 255
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
+model.add(Conv2D(filters=8, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
-model.add(Conv2D(15, (3, 3), activation='relu'))
+model.add(Conv2D(filters=3, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(25, activation='relu'))
+model.add(Dense(units=25, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
