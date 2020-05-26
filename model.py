e6419a815df1f4b099179d310d97a276c6b516c0
sid96020 <sid9602044416@gmail.com>
2020-05-26 09:00:38 +0530

    fifth

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
-num_classes = 10
+num_classes=10
 epochs=1
 
 # input image dimensions
