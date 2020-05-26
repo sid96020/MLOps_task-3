35b24070cb98e05f98e1f5479de6ff8cdd7e11ec
sid96020 <sid9602044416@gmail.com>
2020-05-26 09:43:21 +0530

    Update model.py

--- model.py
+++ model.py
@@ -31,10 +31,8 @@ x_test /= 255
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 model = Sequential()
-model.add(Conv2D(filters=1, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(filters=1,kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D( filters=1 ,kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+model.add(Conv2D( filters=1 ,kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
